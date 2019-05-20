ed0c43a094866359072474b5fb8cadf7e00d7e3f
JereMedr <jeremedrano228@gmail.com>
2019-05-19 22:03:38 -0300

    consulta a la API funcionando

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
-zaca bigote de leche
+function weatherUrl(lat, lon){
+    return `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=dbf230c9bb687fd6db2cecf0ba1b1263`
+}
+
+ function getWeather(lat, lon){
+     return fetch(weatherUrl(lat, lon))
+        .then(r => r.json())
+        .then(weather => console.log(weather))
+ }
+
+ function convertKelvinToCelsius(kelvin) {
+	if (kelvin < (0)) {
+		return 'below absolute zero (0 K)';
+	} else {
+		return (kelvin-273.15);
+	}
+}
+
+ getWeather(-34.5221554, -58.7000067)
+ 
